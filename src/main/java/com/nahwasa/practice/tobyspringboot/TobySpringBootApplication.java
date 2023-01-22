d1f3700308256c9739c60de7a3f38ce644b302fe
nahwasa <nahwasa@gmail.com>
2023-01-23 08:25:05 +0900

    프로젝트 생성

---
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.practice.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringBootApplication.class, args);
+    }
+
+}
