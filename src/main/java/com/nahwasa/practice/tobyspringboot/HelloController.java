900b9456ccef9ac9e1e77d139c311675be58b2d0
nahwasa <nahwasa@gmail.com>
2023-01-23 10:47:21 +0900

    [애노테이션 매핑 정보 사용] 강의와 다르게 @Controller가 포함되어있는데, 스프링부트 3.0대부터는 안붙이면 못찾도록 변경되었기 때문임.

--- src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
@@ -1,7 +1,14 @@
 package com.nahwasa.practice.tobyspringboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;   // 인터페이스지만 스프링이 알아서 컨테이너 뒤져서 알아서 구현체 찾아서 넣어줌.
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name));
--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -1,25 +1,15 @@
 package com.nahwasa.practice.tobyspringboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.io.IOException;
 
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();  // ApplicationContext가 결국 스프링 컨테이너임.
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
