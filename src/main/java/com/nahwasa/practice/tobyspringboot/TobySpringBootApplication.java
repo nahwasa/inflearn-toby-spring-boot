7997b5ee98c90b20a14112f2560701574b7e5ee3
nahwasa <nahwasa@gmail.com>
2023-01-23 11:09:41 +0900

    [자바코드 구성 정보 사용] 팩토리 메서드를 만들고 @Bean으로 등록 가능하도록 변경. GenericWebApplicationContext은 저 @Bean을 인식 불가하므로 AnnotationConfigWebApplicationContext로 변경해줘야 함.

--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -3,13 +3,25 @@ package com.nahwasa.practice.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class TobySpringBootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -23,8 +35,7 @@ public class TobySpringBootApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(TobySpringBootApplication.class);
         applicationContext.refresh();
 
 
