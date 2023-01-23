86e3c36eb0dbcb29baeb2b5968d24ad50a31964f
nahwasa <nahwasa@gmail.com>
2023-01-23 16:29:54 +0900

    [@Configuration과 proxyBeanMethods] (학습테스트) @Configuration에 proxyBeanMethods=false 를 줘서 자바에서의 동작과 동일하게 동작하도록 설정 가능함.

--- src/test/java/com/nahwasa/practice/study/ConfigurationTest.java
+++ src/test/java/com/nahwasa/practice/study/ConfigurationTest.java
@@ -1,12 +1,15 @@
 package com.nahwasa.practice.study;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@EnableScheduling
 public class ConfigurationTest {
     @Test
     void configuration() {
@@ -14,7 +17,7 @@ public class ConfigurationTest {
         Bean1 bean1 = myConfig.bean1();
         Bean2 bean2 = myConfig.bean2();
 
-        assertThat(bean1.common).isSameAs(bean2.common);
+        assertThat(bean1.common).isNotSameAs(bean2.common);
     }
 
     @Test
@@ -26,10 +29,31 @@ public class ConfigurationTest {
         Bean1 bean1 = ac.getBean(Bean1.class);
         Bean2 bean2 = ac.getBean(Bean2.class);
 
+        assertThat(bean1.common).isNotSameAs(bean2.common);
+    }
+
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
         assertThat(bean1.common).isSameAs(bean2.common);
     }
 
-    @Configuration
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) this.common = super.common();
+
+            return this.common;
+        }
+    }
+
+    @Configuration(proxyBeanMethods = false)
     static class MyConfig {
         @Bean
         Common common() {
