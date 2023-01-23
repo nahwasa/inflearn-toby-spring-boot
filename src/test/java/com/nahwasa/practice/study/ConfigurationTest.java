0ea34751f52773fc17a76d73bbc56bb653c60892
nahwasa <nahwasa@gmail.com>
2023-01-23 16:19:01 +0900

    [@Configuration과 proxyBeanMethods] (학습테스트) 스프링 밖에서 자바단에서 예상한바와 스프링 내에서 동작하는 것과 서로 다르다.

---
+++ src/test/java/com/nahwasa/practice/study/ConfigurationTest.java
@@ -0,0 +1,69 @@
+package com.nahwasa.practice.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationTest {
+    @Test
+    void configuration() {
+        MyConfig myConfig = new MyConfig();
+        Bean1 bean1 = myConfig.bean1();
+        Bean2 bean2 = myConfig.bean2();
+
+        assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Test
+    void configuration2() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+
+        assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Configuration
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+
+    }
+}
