4d1d344ef727ff76b9382a2b9496b2caf818dcff
nahwasa <nahwasa@gmail.com>
2023-01-23 13:13:49 +0900

    [Bean의 생명주기 메소드] DispatcherServlet이 사용하는 ApplicationContext를 스프링 컨테이너가 넣어줌. 실제로 넣어주는지 확인하기 위해 HelloController에서도 ApplicationContext롤 생성자 주입으로 받아봄.

--- src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
@@ -1,5 +1,8 @@
 package com.nahwasa.practice.tobyspringboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,11 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;   // 인터페이스지만 스프링이 알아서 컨테이너 뒤져서 알아서 구현체 찾아서 넣어줌.
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
