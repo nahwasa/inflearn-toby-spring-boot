e2a95a887b02f969a42d035d97e492a56a69a324
nahwasa <nahwasa@gmail.com>
2023-01-23 10:50:27 +0900

    [애노테이션 매핑 정보 사용] RestController를 쓰면 DispatcherServlet이 @ResponseBody가 모두 붙어있다고 생각하고 동작하므로 이렇게 바꿔도 동일함.

--- src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
@@ -17,7 +18,6 @@ public class HelloController {
     }
 
     @GetMapping
-    @ResponseBody
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name));
