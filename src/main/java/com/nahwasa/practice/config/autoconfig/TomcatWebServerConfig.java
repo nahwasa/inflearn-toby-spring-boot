67dfc7e29b057cc7f6dbe3db91bab03723b140e8
nahwasa <nahwasa@gmail.com>
2023-01-23 21:26:09 +0900

    [자동 구성 정보 대체하기] 사용자 구성정보에 설정을 하게되면 자동 구성 정보쪽은 알아서 설정을 안하게 되도록 세팅

--- src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.nahwasa.practice.config.autoconfig;
 
 import com.nahwasa.practice.config.ConditionalMyOnClass;
 import com.nahwasa.practice.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean   // 동일한 빈이 사용자 구성정보에서 안떠있었다면 진행
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
