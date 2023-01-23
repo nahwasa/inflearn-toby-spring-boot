de36f3c31d393b3a58f32cd172588d57a473c371
nahwasa <nahwasa@gmail.com>
2023-01-23 17:39:28 +0900

    [@Conditional과 @Condition] 스프링부트 3.0 이상이라면 gradle에 설정 해줘야함. (제티는 현재 서블릿 6.0을 지원하지 않아서 5.0으로 낮춰야함)

--- src/main/java/com/nahwasa/practice/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/JettyWebServerConfig.java
@@ -5,11 +5,24 @@ import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
--- src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
@@ -4,11 +4,24 @@ import com.nahwasa.practice.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
