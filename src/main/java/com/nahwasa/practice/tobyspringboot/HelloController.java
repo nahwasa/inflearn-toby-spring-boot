17b42307b6913b47f28acfc40e3507943ae8c2a0
nahwasa <nahwasa@gmail.com>
2023-01-23 09:51:04 +0900

    [의존 오브젝트 추가]

--- src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
@@ -1,10 +1,11 @@
 package com.nahwasa.practice.tobyspringboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Objects;
 
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/nahwasa/practice/tobyspringboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.nahwasa.practice.tobyspringboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
