9c4faa5cd27ce8384a40b6580f2b456a74019bd1
nahwasa <nahwasa@gmail.com>
2023-01-23 11:17:51 +0900

    [@Coponent 스캔] @Bean대신 @ComponentScan을 추가하고 Bean 등록될 애들 위에 @Component 붙이기. 다만 강의와 달리 스프링부트 3.0대에서는 컨트롤러에 @Component만으론 동작 안되고, @Controller나 @RestController를 붙여줘야 함.

--- src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/HelloController.java
@@ -1,12 +1,14 @@
 package com.nahwasa.practice.tobyspringboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
@@ -16,6 +18,7 @@ public class HelloController {
     }
 
     @GetMapping
+    @ResponseBody
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name));
--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -3,22 +3,14 @@ package com.nahwasa.practice.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class TobySpringBootApplication {
-    @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
-    }
 
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
