05ec5b2f636c5b6e6c6e0b5d040cfbd267bc3baf
nahwasa <nahwasa@gmail.com>
2023-01-23 15:12:23 +0900

    [합성 애노테이션의 적용] @Bean으로 사용하던 애들도 Config로 따로 뺌. 이제 근본 스프링 부트랑 비슷해짐.

--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -1,22 +1,9 @@
 package com.nahwasa.practice.tobyspringboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @MySpringBootAnnotation
 public class TobySpringBootApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(TobySpringBootApplication.class, args);
