ea75eafe454f7b33f0f45c4a609ef5dadb7e35c8
nahwasa <nahwasa@gmail.com>
2023-01-23 16:02:59 +0900

    [자동 구성 애노테이션 적용]

--- src/main/java/com/nahwasa/practice/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.nahwasa.practice.config.autoconfig;
 
+import com.nahwasa.practice.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
--- src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,11 @@
 package com.nahwasa.practice.config.autoconfig;
 
+import com.nahwasa.practice.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
