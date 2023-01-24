f1d4782e7b3dde526815478f27842943421fdefe
nahwasa <nahwasa@gmail.com>
2023-01-24 13:18:30 +0900

    [DataSource 자동 구성 클래스] Hikari 데이터 소스 추가

--- src/main/java/com/nahwasa/practice/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/DataSourceConfig.java
@@ -3,6 +3,8 @@ package com.nahwasa.practice.config.autoconfig;
 import com.nahwasa.practice.config.ConditionalMyOnClass;
 import com.nahwasa.practice.config.EnableMyConfigurationProperties;
 import com.nahwasa.practice.config.MyAutoConfiguration;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
@@ -14,6 +16,21 @@ import java.sql.Driver;
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
 public class DataSourceConfig {
     @Bean
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    @ConditionalOnMissingBean   // 혹시 사용자 영역에서 히카리를 설정할수도있으므로 설정해둔거.
+    DataSource hikariDataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        HikariDataSource dataSource = new HikariDataSource();   // pool 이 없고 매번 새로운 커넥션을 만드는 정말 심플한 데이터소스임
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
     DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();   // pool 이 없고 매번 새로운 커넥션을 만드는 정말 심플한 데이터소스임
 
