59d699401ca15ca5ec1cc25d3ff752701976a681
nahwasa <nahwasa@gmail.com>
2023-01-24 11:21:59 +0900

    [@Value와 PropertySourcesPlaceholderConfigurer]

--- src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/TomcatWebServerConfig.java
@@ -2,20 +2,25 @@ package com.nahwasa.practice.config.autoconfig;
 
 import com.nahwasa.practice.config.ConditionalMyOnClass;
 import com.nahwasa.practice.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@Component
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean   // 동일한 빈이 사용자 구성정보에서 안떠있었다면 진행
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(contextPath);
 
         return factory;
     }
