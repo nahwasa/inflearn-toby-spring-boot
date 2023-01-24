8b9180b869ab1857a5b37420b075e68e00d7f5ae
nahwasa <nahwasa@gmail.com>
2023-01-24 18:29:05 +0900

    [자동 구성 조건 결과 확인] 자동 구성되는 빈들 목록을 띄울 수 있도록 설정

--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -1,8 +1,11 @@
 package com.nahwasa.practice.tobyspringboot;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
@@ -16,6 +19,23 @@ public class TobySpringBootApplication {
     @PostConstruct
     void init() {
         jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Bean
+    ApplicationRunner run(ConditionEvaluationReport report) {
+        return args -> {
+            System.out.println(report.getConditionAndOutcomesBySource().entrySet().stream()
+                    .filter(co -> co.getValue().isFullMatch())
+                    .filter(co -> co.getKey().indexOf("Jmx") < 0)
+                    .map(co -> {
+                        System.out.println(co.getKey());
+                        co.getValue().forEach(c -> {
+                            System.out.println("\t" + c.getOutcome());
+                        });
+                        System.out.println();
+                        return co;
+                    }).count());
+        };
     }
 
     public static void main(String[] args) {
