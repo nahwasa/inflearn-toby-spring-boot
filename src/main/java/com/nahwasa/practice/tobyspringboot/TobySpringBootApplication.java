f4d326b49fb13780c9f7826d7d2d6498e241e240
nahwasa <nahwasa@gmail.com>
2023-01-24 10:34:08 +0900

    [자동 구성에 Environment 프로퍼티 적용] 사용자 구성 영역에 있던 톰캣서버 제거, ApplicationRunner 구현체는 빈이 모두 생성된 후 동작한다. .properties에 작성한 것 보다 환경 변수로 등록한게 더 우선순위가 높다.

--- src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/nahwasa/practice/tobyspringboot/TobySpringBootApplication.java
@@ -1,10 +1,21 @@
 package com.nahwasa.practice.tobyspringboot;
 
 import com.nahwasa.practice.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobySpringBootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TobySpringBootApplication.class, args);
