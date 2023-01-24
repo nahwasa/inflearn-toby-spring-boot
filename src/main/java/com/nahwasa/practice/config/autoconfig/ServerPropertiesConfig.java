ef84fa310e664f2470da88c19afbe55368fee41d
nahwasa <nahwasa@gmail.com>
2023-01-24 11:43:49 +0900

    [프로퍼티 클래스의 분리] 매번 ServerPropertiesConfig 에 프로퍼티 추가될 때 마다 넣어줄 수 없으니 유틸리티로 알아서 얻어오도록 변경

--- src/main/java/com/nahwasa/practice/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/com/nahwasa/practice/config/autoconfig/ServerPropertiesConfig.java
@@ -1,6 +1,7 @@
 package com.nahwasa.practice.config.autoconfig;
 
 import com.nahwasa.practice.config.MyAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -9,11 +10,6 @@ public class ServerPropertiesConfig {
 
     @Bean
     public ServerProperties serverProperties(Environment environment) {
-        ServerProperties properties = new ServerProperties();
-
-        properties.setContextPath(environment.getProperty("contextPath"));
-        properties.setPort(Integer.parseInt(environment.getProperty("port")));
-
-        return properties;
+        return Binder.get(environment).bind("", ServerProperties.class).get();
     }
 }
